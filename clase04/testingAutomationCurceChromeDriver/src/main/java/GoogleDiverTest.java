78fa22006836c40862e72902d93b09ecf41b0eaa
rodixxi <rodixxi@gmail.com>
2017-06-28 08:12:53 -0300

    Nuevos ejercicios

--- clase04/testingAutomationCurceChromeDriver/src/main/java/GoogleDiverTest.java
+++ clase04/testingAutomationCurceChromeDriver/src/main/java/GoogleDiverTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -23,6 +24,7 @@ public class GoogleDiverTest {
     protected String GOOGLE_SEARCH_BUTTTON = "_fZl";
     protected By WIKIPEDIA_LINK = By.xpath("//div[@id='rso']/div/div/div/div/div/h3/a");
     protected String WIKIPEDIA_TITLE ="Cats - Wikipedia, la enciclopedia libre";
+    protected String CATS_TITLE ="CATS: Crash Arena Turbo Stars - Aplicaciones de Android en Google Play";
     protected String WORDS_TO_SEARCH_IN_GOOGLE= "CATS";
 
     @BeforeClass // Setting chromedriver driver
@@ -31,10 +33,12 @@ public class GoogleDiverTest {
         System.setProperty("webdriver.chrome.driver", "/home/rodixxi/Repositories/testingAutomation_Java_H_A_2017/chromedriver");
                 //Disable barInfo
                 ChromeOptions options = new ChromeOptions();
-        options.addArguments("disable-infobars");
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        options.addArguments("incognito");
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
         driver = new ChromeDriver(options);
     }
-
+    /*
     @Test
     // Ejecuting Test
     public void verifyWikepediaPageTittle() {
@@ -56,11 +60,52 @@ public class GoogleDiverTest {
         getTitle = driver.getTitle();
         Assert.assertEquals(getTitle, WIKIPEDIA_TITLE);
     }
+    */
+    @Test
+    // Ejecuting Test
+    public void verifyCatsPageTittle() {
+        //Open Google
+        driver.get(appURL);
+        //Verify Google Title
+        String getTitle = driver.getTitle();
+        Assert.assertEquals(getTitle, GOOGLE_TITLE);
+        //Fill out search input
+        driver.findElement(By.id(GOOGLE_INPUT_ID)).sendKeys(WORDS_TO_SEARCH_IN_GOOGLE);
+        //Press search button
+        driver.findElement(By.id(GOOGLE_SEARCH_BUTTTON)).click();
+        //Wait till GOOGLE_IMAGES_LINK is present
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(WIKIPEDIA_LINK));
+        //Press search button
+        driver.findElement(WIKIPEDIA_LINK).click();
+        //Verify Wikipedia Title
+        getTitle = driver.getTitle();
+        Assert.assertEquals(getTitle, CATS_TITLE);
+    }
+
+    @Test
+    public void scenario1(){
+        driver.get(appURL);
+        //Verify Google Title
+        String getTitle = driver.getTitle();
+        Assert.assertEquals(getTitle, GOOGLE_TITLE);
+        //Fill out search input
+        driver.findElement(By.id(GOOGLE_INPUT_ID)).sendKeys("https://accounts.google.com");
+        //Press search button
+        driver.findElement(By.id(GOOGLE_SEARCH_BUTTTON)).click();
+        //Wait till GOOGLE_IMAGES_LINK is present
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(WIKIPEDIA_LINK));
+        //Press search button
+        driver.findElement(WIKIPEDIA_LINK).click();
+        getTitle = driver.getTitle();
+        Assert.assertEquals(getTitle, "My Account");
+    }
 
     @AfterClass
     // Closing Browser when finish the test
     public void tearDown() {
-        //driver.quit();
+        driver.quit();
     }
 
 
